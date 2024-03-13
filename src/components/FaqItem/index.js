3128a62630bd45337eae93f4763e767b0b232d8a
Prashant Tiwari <149590314+PrashnatTiwari@users.noreply.github.com>
2024-03-13 17:12:57 +0530

    Update index.js

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,34 +1,49 @@
+import {Component} from 'react'
+
 import './index.css'
 
-const FaqItem = props => {
-  const {itemDetails, deleteImage} = props
-  const {id, questionText, answerText} = itemDetails
-  const {isClicked} = deleteImage
-
-  const imgUrl = isClicked
-    ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
-    : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
-
-  const onClickIcon = () => {
-    deleteImage(id)
+class FaqItem extends Component {
+  state = {
+    isClicked: false,
   }
 
-  return (
-    <li className="list-item">
-      <div className="flex-container">
-        <h1 className="question-text-heading">{questionText}</h1>
-        <img
-          src={imgUrl}
-          className="plus-image"
-          alt="plus"
-          onClick={onClickIcon}
-        />
-      </div>
-      <hr className="horizontal-line" />
+  deleteImage = () => {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked,
+    }))
+  }
 
-      {isClicked && <p className="answer-text-paragraph">{answerText}</p>}
-    </li>
-  )
+  render() {
+    const {itemDetails} = this.props
+    const {questionText, answerText} = itemDetails
+    const {isClicked} = this.state
+    const imgUrl = isClicked
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+
+    const altText = isClicked ? 'minus' : 'plus'
+
+    return (
+      <li className="list-item">
+        <div className="flex-container">
+          <h1 className="question-text-heading">{questionText}</h1>
+          <img
+            src={imgUrl}
+            className="plus-image"
+            alt={altText}
+            onClick={this.deleteImage}
+          />
+        </div>
+
+        {isClicked && (
+          <>
+            <hr className="horizontal-line" />
+            <p className="answer-text-paragraph">{answerText}</p>
+          </>
+        )}
+      </li>
+    )
+  }
 }
 
 export default FaqItem
